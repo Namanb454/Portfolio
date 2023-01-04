9e1022f7598d50df4448774104b16e52420b39c6
Naman <namanb454@gmail.com>
2023-01-05 04:25:04 +0530

    finalcommit9

--- components/Main.js
+++ components/Main.js
@@ -25,7 +25,7 @@ const Main = () => {
               You can reach me through my handles given at the bottom.</p>
             <div className='flex justify-center'>
               <a href='NamanResume.pdf'>
-                <button className='text-white bg-red-500 border-red-400 py-2 px-6 focus:outline-none hover:bg-red-400 rounded text-lg'>Resume</button>
+                <button className='text-white bg-red-500 border-red-400 py-2 px-6 focus:outline-none hover:bg-red-400 rounded text-lg animate-pulse'>Resume</button>
               </a>
 
             </div>
