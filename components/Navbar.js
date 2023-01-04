d5a485af6a807b5b7bc681a23462ab18b9384e17
Naman <namanb454@gmail.com>
2023-01-04 13:43:11 +0530

    finalcommit4

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,39 +7,37 @@ const Navbar = (props) => {
         width: "100%",
         top: "0"
     }
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+          x.className += " responsive";
+        } else {
+          x.className = "topnav";
+        }
+      }
 
     return (
         <div>
-            <img style={navbarbg} src='navbarbg3.jpg' />
+            <img style={navbarbg} src='navbar1.jpg' />
 
-           
-<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
-  <div class="container flex flex-wrap items-center justify-between mx-auto">
-    <Link href="/" class="flex items-center">
-        <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-        <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Naman Bansal</span>
-    </Link>
-    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-      <span class="sr-only">Open main menu</span>
-      <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-    </button>
-    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <li>
-        <Link href="/" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Home</Link>
-        </li>
-        <li>
-          <Link href="Skills" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Skills</Link>
-        </li>
-        <li>
-          <Link href="Contact" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</Link>
-        </li>
-        
-        
-      </ul>
-    </div>
-  </div>
-</nav>
+            <header className="bg-red-500 text-gray-600 body-font shadow-2xl">
+                <div className=" container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                    <Link href="/" className=" flex title-font font-medium items-center text-white mb-4 md:mb-0">
+                        <img className='w-14 ' src='pic.jpg' />
+                        <span className="ml- text-white font-semibold text-xl">Naman Bansal</span>
+                        
+                    </Link>
+                    
+                    <nav className=" text-white md:ml-96 ml-7 flex flex-wrap items-center text-base justify-center">
+                        <Link href="/" className="mr-5 font-serif hover:text-red-100"> Home </Link>
+                        <Link href="/Skills" className="mr-5 font-serif hover:text-red-100">Skills</Link>
+                        <Link href="/Contact" className="mr-5 font-serif hover:text-red-100">Contact</Link>
+                 
+                    </nav>
+                    
+
+                </div>
+            </header>
 
         </div>
     )
