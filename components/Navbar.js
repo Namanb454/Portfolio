a7752ad5877b1568bc8fab2610447df758bd7870
Naman <namanb454@gmail.com>
2023-01-04 11:46:25 +0530

    finalcommit

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
-const Navbar = () => {
+
+const Navbar = (props) => {
     const navbarbg = {
         height: "5rem",
         width: "100%",
@@ -13,15 +14,16 @@ const Navbar = () => {
 
             <header className="bg-red-500 text-gray-600 body-font shadow-2xl">
                 <div className=" container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                    <a className=" flex title-font font-medium items-center text-white mb-4 md:mb-0">
+                    <Link href="/" className=" flex title-font font-medium items-center text-white mb-4 md:mb-0">
                         <img className='w-14 ' src='pic.jpg' />
                         <span className="ml- text-white font-semibold text-xl">Naman Bansal</span>
-                    </a>
+                        
+                    </Link>
                     <nav className=" text-white md:ml-96 ml-7 flex flex-wrap items-center text-base justify-center">
-                        <a href="/" className="mr-5 font-serif hover:text-red-100">Home</a>
-                        <a href="/Skills" className="mr-5 font-serif hover:text-red-100">Skills</a>
-                        <a href="/Contact" className="mr-5 font-serif hover:text-red-100">Contact</a>
-                        {/* <a className="mr-5 hover:text-gray-900">Fourth Link</a> */}
+                        <Link href="/" className="mr-5 font-serif hover:text-red-100"> Home </Link>
+                        <Link href="/Skills" className="mr-5 font-serif hover:text-red-100">Skills</Link>
+                        <Link href="/Contact" className="mr-5 font-serif hover:text-red-100">Contact</Link>
+                 
                     </nav>
                     
 
